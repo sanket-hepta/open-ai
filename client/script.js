c486bf2451addf4802473705980fcc4562e6f9f2
Sanket <sanket.hepta@gmail.com>
2023-04-10 19:02:54 +0530

    Changes in files

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId);
     loader(messageDiv);
 
-    const response = await fetch(`http://localhost:5000`, {
+    const response = await fetch(`https://open-ai-40mm.onrender.com`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
